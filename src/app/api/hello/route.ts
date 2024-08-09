7e98258d7832583ad68702acf20eabf37d221839
Anni Wang <anni@cloudflare.com>
2024-08-09 15:05:30 -0400

    changes

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
   // await myKv.put('suffix', ' from a KV store!')
   // const suffix = await myKv.get('suffix')
   // responseText += suffix
-  //new comment sss
+  //new comment ssssss
 
   return new Response(responseText)
 }
